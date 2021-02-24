01dd34a73be498d8a187b48b0309905c3b674a84
Hugo-NF <hugonfonseca@hotmail.com>
2021-02-24 00:31:09 -0300

    Attempt to fix Jest

--- __tests__/App.tsx
+++ __tests__/App.tsx
@@ -1,10 +1,14 @@
-import 'react-native';
 import React from 'react';
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
+import { render } from 'react-native-testing-library';
 import App from '../src/App';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+const createTestProps = (props?: object) => ({
+  ...props,
+});
+describe('App', () => {
+  const props = createTestProps();
+  const { getByText } = render(<App {...props} />);
+  it('should render a welcome', () => {
+    expect(getByText(/welcome/i)).toBeDefined();
+  });
 });
