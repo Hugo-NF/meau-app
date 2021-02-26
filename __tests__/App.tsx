b10bd3a251bd9dc792df7403162caf4b1b0cb0d1
labm1997 <labm1997@gmail.com>
2021-02-25 22:56:30 -0300

    Correçao do jest

--- __tests__/App.tsx
+++ __tests__/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-native-testing-library';
 import App from '../src/App';
 
+jest.useFakeTimers();
+
 const createTestProps = (props?: object) => ({
   ...props,
 });
@@ -9,6 +11,6 @@ describe('App', () => {
   const props = createTestProps();
   const { getByText } = render(<App {...props} />);
   it('should render a welcome', () => {
-    expect(getByText(/welcome/i)).toBeDefined();
+    expect(getByText(/working/i)).toBeDefined();
   });
 });
