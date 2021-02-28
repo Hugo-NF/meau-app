07655df9ab2fa683070d212af6a716f16528ee30
Hugo-NF <hugonfonseca@hotmail.com>
2021-02-28 19:14:56 -0300

    💄 Add button sample to home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 
-import { setStatusBarStyle, StatusBar } from 'expo-status-bar';
 import { Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
+import AsyncButton from '../../components/AsyncButton';
+import { Theme } from '../../constants';
 import { Container } from './styles';
 
 export default function Home() : JSX.Element {
-  setStatusBarStyle('auto');
-
   const navigation = useNavigation();
 
   return (
     <Container>
       <Text onPress={() => navigation.navigate('Login')}>Bugstenium rocks!</Text>
-      <StatusBar />
+      <AsyncButton
+        styles={{
+          flex: 1,
+          width: '70%',
+          height: '32px',
+          backgroundColor: Theme.default.secondary,
+          borderRadius: '5px',
+          marginTop: '10px',
+        }}
+        asyncAction={false}
+        callback={() => {
+          navigation.navigate('Login');
+        }}
+      >
+        <Text>Login</Text>
+      </AsyncButton>
     </Container>
   );
 }
