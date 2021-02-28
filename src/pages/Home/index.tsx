1bb5833479b726671ad48c83f986670fad970e12
Hugo-NF <hugonfonseca@hotmail.com>
2021-02-28 19:28:06 -0300

    💄 Sample with icon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,8 @@ import { Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
+import { Entypo } from '@expo/vector-icons';
+
 import AsyncButton from '../../components/AsyncButton';
 import { Theme } from '../../constants';
 import { Container } from './styles';
@@ -17,7 +19,7 @@ export default function Home() : JSX.Element {
       <AsyncButton
         styles={{
           flex: 1,
-          width: '70%',
+          width: '30%',
           height: '32px',
           backgroundColor: Theme.default.secondary,
           borderRadius: '5px',
@@ -28,6 +30,7 @@ export default function Home() : JSX.Element {
           navigation.navigate('Login');
         }}
       >
+        <Entypo name="lock" size={16} />
         <Text>Login</Text>
       </AsyncButton>
     </Container>
