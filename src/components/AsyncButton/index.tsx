05cd7285eb67d6fe6b235b261779b218056ec2d8
Hugo-NF <hugonfonseca@hotmail.com>
2021-02-28 18:52:49 -0300

    🎨 Add first version of app colors theme

---
+++ src/components/AsyncButton/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+import { ActivityIndicator, GestureResponderEvent } from 'react-native';
+import { Button, ButtonType } from './styles';
+import { Theme } from '../../constants';
+
+interface IButtonProps extends ButtonType {
+  children: React.ReactNode,
+  asyncAction: boolean,
+  callback: <GestureResponderEvent>(arg: GestureResponderEvent) => void,
+  activityIndicator?: Record<string, unknown>,
+  styles?: Record<string, unknown>,
+}
+
+const AsyncButton : React.FC<IButtonProps> = (props : IButtonProps) => {
+  const [disabled, setDisabled] = useState(false);
+  const {
+    activityIndicator, asyncAction, children, callback, styles,
+  } = props;
+
+  let onPressCallback;
+  if (asyncAction) {
+    onPressCallback = async (event: GestureResponderEvent) => {
+      setDisabled(true);
+      await callback(event);
+      setDisabled(false);
+    };
+  } else {
+    onPressCallback = callback;
+  }
+
+  return (
+    <Button
+      onPress={onPressCallback}
+      disabled={disabled}
+      {...styles}
+    >
+      {(asyncAction && disabled) && <ActivityIndicator {...activityIndicator} />}
+      {!(asyncAction && disabled) && children}
+    </Button>
+  );
+};
+
+AsyncButton.defaultProps = {
+  activityIndicator: {
+    size: 'small',
+    color: Theme.default.light,
+  },
+  styles: {
+    flexDirection: 'row',
+    backgroundColor: Theme.default.primary,
+    marginTop: '0px',
+    marginBottom: '0px',
+    marginLeft: '0px',
+    marginRight: '0px',
+    borderRadius: '0px',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
+
+export default AsyncButton;
