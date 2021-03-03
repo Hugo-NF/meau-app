214444c0d39cebcab7f3fcc194508f547b418949
Hugo-NF <hugonfonseca@hotmail.com>
2021-03-03 19:47:47 -0300

    🚧 Move MockedNavigation path

--- src/components/TextInputCheck/index.tsx
+++ src/components/TextInputCheck/index.tsx
@@ -13,14 +13,14 @@ const styles = StyleSheet.create({
   },
   textInput: {
     fontSize: 14,
-    fontFamily: 'Roboto Regular',
+    fontFamily: 'Roboto_400Regular',
     padding: 0,
     width: 328 - 20,
     flexGrow: 1,
   },
   check: {
     width: 20,
-    fontFamily: 'Roboto Regular',
+    fontFamily: 'Roboto_400Regular',
     color: '#434343',
     paddingLeft: 5,
   },
