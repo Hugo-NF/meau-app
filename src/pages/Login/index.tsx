e004bc44a9b285c50c1e441791d48a4818440638
Hugo-NF <hugonfonseca@hotmail.com>
2021-03-14 20:47:38 -0300

    ✨ Formik added to login form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,32 +1,39 @@
+/* eslint-disable no-template-curly-in-string */
 // Package imports.
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useLayoutEffect } from 'react';
 
 import { setStatusBarBackgroundColor } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
 import auth, { firebase } from '@react-native-firebase/auth';
 
 import * as Yup from 'yup';
-import { Formik } from 'formik';
+import { ErrorMessage, Formik } from 'formik';
+
+import { HelperText, TextInput } from 'react-native-paper';
 
 // Style imports.
-import { styles, styledComponents } from './styles';
+import {
+  ButtonText,
+  Container,
+  LoginForm,
+  SubmitButton,
+} from './styles';
 
 // Layout import
 import HeaderLayout from '../../layouts/HeaderLayout';
 
-// Component imports.
-import AsyncButton from '../../components/AsyncButton';
-import TextInputCheck from '../../components/TextInputCheck';
+// Project imports.
 import { Theme } from '../../constants';
+
+interface LoginForm {
+  email: string,
+  password: string,
+}
 
 // Component export.
 export default function Login() : JSX.Element {
   // Variable declaration.
   const navigation = useNavigation();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [wrongLogin, setWrongLogin] = useState(false);
 
   if (auth().currentUser) {
     navigation.navigate('Authorized');
@@ -37,32 +44,20 @@ export default function Login() : JSX.Element {
     setStatusBarBackgroundColor(Theme.elements.statusBarPrimary, true);
   }, [navigation]);
 
-  // Functions.
-  const notEmpty = (text: string) : boolean => text !== '';
-
-  const placeholderFunction = () : null => null;
-
-  // Styled components.
-  const { ButtonText, Container, LoginForm } = styledComponents;
-
-  const signIn = async (): Promise<void> => {
+  const signIn = async ({ email, password }: LoginForm): Promise<void> => {
     console.log(email);
-    try {
-      if (!email || !password) {
-        console.log('Campos vazios');
-        return;
-      }
-      const response = await auth().signInWithEmailAndPassword(email, password);
-      console.log(response);
-      if (response && response.user) {
-        // Autenticou
-        navigation.navigate('Authorized');
-      }
-    } catch (e) {
-      // Erro
-      console.log(e);
-      setWrongLogin(true);
-    }
+    console.log(password);
+    // try {
+    //   const response = await auth().signInWithEmailAndPassword(email, password);
+    //   console.log(response);
+    //   if (response && response.user) {
+    //     // Autenticou
+    //     navigation.navigate('Authorized');
+    //   }
+    // } catch (e) {
+    //   // Erro
+    //   console.log(e);
+    // }
   };
 
   return (
@@ -83,27 +78,81 @@ export default function Login() : JSX.Element {
       }}
     >
       <Container>
-        <LoginForm>
-          <TextInputCheck
-            containerStyle={styles.textInputContainer}
-            textInputStyle={styles.textInput}
-            validation={notEmpty}
-            placeholder="Nome de usuário"
-          />
-          <TextInputCheck
-            containerStyle={styles.textInputContainer}
-            textInputStyle={styles.textInput}
-            validation={notEmpty}
-            placeholder="Senha"
-          />
-          <AsyncButton
-            styles={styles.asyncButton}
-            asyncAction={false}
-            callback={placeholderFunction}
-          >
-            <ButtonText>Entrar</ButtonText>
-          </AsyncButton>
-        </LoginForm>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string().required('E-mail é obrigatório').email('Deve ser um e-mail válido'),
+            password: Yup.string().required('Senha é obrigatória').min(6, 'Deve conter pelo menos ${min} caracteres'),
+          })}
+          onSubmit={(data) => signIn(data)}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            touched,
+            errors,
+            isSubmitting,
+          }) => (
+            <LoginForm>
+              <TextInput
+                label="E-mail"
+                placeholder="E-mail"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                mode="flat"
+                keyboardType="email-address"
+                dense
+                error={Boolean(touched.email && errors.email)}
+                selectionColor={Theme.elements.statusBarPrimary}
+                underlineColor={Theme.elements.headerText}
+                style={{
+                  backgroundColor: 'transparent',
+                  maxHeight: 56,
+                }}
+              />
+              <HelperText
+                type="error"
+                visible={Boolean(touched.email && errors.email)}
+              >
+                {touched.email && errors.email}
+              </HelperText>
+              <TextInput
+                label="Senha"
+                placeholder="Senha"
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                mode="flat"
+                error={Boolean(touched.password && errors.password)}
+                secureTextEntry
+                selectionColor={Theme.elements.statusBarPrimary}
+                underlineColor={Theme.elements.headerText}
+                style={{
+                  backgroundColor: 'transparent',
+                  maxHeight: 56,
+                }}
+              />
+              <HelperText
+                type="error"
+                visible={Boolean(touched.password && errors.password)}
+              >
+                {touched.password && errors.password}
+              </HelperText>
+              <SubmitButton
+                disabled={isSubmitting}
+                onPress={handleSubmit as (values: unknown) => void}
+              >
+                <ButtonText>Entrar</ButtonText>
+              </SubmitButton>
+            </LoginForm>
+          )}
+        </Formik>
       </Container>
     </HeaderLayout>
   );
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,55 +1,35 @@
 // Package imports.
-import { TextProps, ViewProps } from 'react-native';
+import { TouchableOpacityProps, TextProps, ViewProps } from 'react-native';
 import styled from 'styled-components/native';
 
 // Style imports.
 import { Theme } from '../../constants';
 
-// Styled components.
-export const styledComponents = {
-  ButtonText: styled.Text<TextProps>`
-    color: ${Theme.elements.buttonText};
-    font-family: "Roboto_400Regular";
-    font-size: 12px;
-    text-transform: uppercase;
-  `,
+export const ButtonText = styled.Text<TextProps>`
+  color: ${Theme.elements.buttonText};
+  font-family: "Roboto_400Regular";
+  font-size: 12px;
+  text-transform: uppercase;
+`;
 
-  Container: styled.View<ViewProps>`
-    flex: 1;
-    align-items: center;
-    background-color: ${Theme.default.background};
-  `,
+export const Container = styled.View<ViewProps>`
+  flex: 1;
+  align-items: center;
+  background-color: ${Theme.default.background};
+`;
 
-  LoginForm: styled.View<ViewProps>`
-    margin-top: 56px;
-    margin-bottom: 40px;
-  `,
-};
+export const LoginForm = styled.View<ViewProps>`
+  margin-top: 56px;
+  margin-bottom: 40px;
+`;
 
-// Styles.
-export const styles = {
-  asyncButton: {
-    backgroundColor: Theme.default.primary,
-    borderRadius: '2px',
-    height: '40px',
-    marginTop: '0px',
-    width: '232px',
-  },
-
-  headerLeftIcon: {
-    color: Theme.elements.buttonText,
-    marginBottom: 16,
-    marginLeft: 16,
-    marginTop: 16,
-  },
-
-  textInput: {
-    width: 288,
-  },
-
-  textInputContainer: {
-    width: 312,
-    marginBottom: 12,
-    marginTop: 0,
-  },
-};
+export const SubmitButton = styled.TouchableOpacity<TouchableOpacityProps>`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${Theme.default.primary};
+  border-radius: 2px;
+  max-height: 40px;
+  margin-top: 32px;
+  width: 232px;
+`;
