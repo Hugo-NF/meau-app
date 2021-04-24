ad4faf31fcbad1d4455a212063587ad3f5166d06
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2021-04-23 22:10:04 -0300

    Removed default profile image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,11 +1,9 @@
 const AnimalPicturePlaceholder = require('../assets/animal_picture_placeholder.png');
 const MeauWhite = require('../assets/meau_logo.png');
 const MeauBlue = require('../assets/meau_logo_2.png');
-const ProfileImage = require('../assets/profile_placeholder.png');
 
 export default {
   AnimalPicturePlaceholder,
   MeauWhite,
   MeauBlue,
-  ProfileImage,
 };
