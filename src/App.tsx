86ecc24664067370e5c0425e1ec84da50706e301
Hugo-NF <hugonfonseca@hotmail.com>
2021-05-18 12:08:13 -0300

    🚧 Add splashscreen (again!)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 // Package imports.
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React, { useState, useCallback, useEffect } from 'react';
+import { View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
 
 // React Navigation Stack.
 import 'react-native-gesture-handler';
@@ -33,25 +35,62 @@ const theme = {
   },
 };
 
-export default function App() : JSX.Element {
+export default function App() : JSX.Element | null {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded] = useFonts({
     Courgette_400Regular,
     Roboto_400Regular,
     Roboto_500Medium,
   });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  useEffect(() => {
+    async function prepare(): Promise<void> {
+      try {
+        // Keep the splash screen visible while we fetch resources
+
+        // Pre-load fonts
+        while (!fontsLoaded); // Waits useFonts to load
+
+        // Make any API calls you need to do here
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this when ready to approve the PR! @AndreLaranjeira
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
-    <GlobalState>
-      <PaperProvider theme={theme}>
-        <NavigationContainer>
-          <StatusBar translucent />
-          <Routes />
-        </NavigationContainer>
-      </PaperProvider>
-    </GlobalState>
+    <View onLayout={onLayoutRootView}>
+      <GlobalState>
+        <PaperProvider theme={theme}>
+          <NavigationContainer>
+            <StatusBar translucent />
+            <Routes />
+          </NavigationContainer>
+        </PaperProvider>
+      </GlobalState>
+    </View>
   );
 }
